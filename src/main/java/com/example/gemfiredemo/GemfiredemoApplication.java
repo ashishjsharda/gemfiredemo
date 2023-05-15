87a6a7681d38e19186d5692399388558b40fdbb7
Ashish Sharda <ashishjsharda@gmail.com>
2023-05-15 07:35:19 -0500

    first commit

---
+++ src/main/java/com/example/gemfiredemo/GemfiredemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gemfiredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemfiredemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GemfiredemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gemfiredemo/GemfiredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gemfiredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemfiredemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
